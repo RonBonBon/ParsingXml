1b0e7b83f12d62a85c306e2f28293c5afe5d0bcc
RonBonBon <ron-aricha@013.net>
2017-06-12 17:16:55 +0300

    Final 06

--- app/src/main/java/hackeru/edu/parsingxml/MainActivity.java
+++ app/src/main/java/hackeru/edu/parsingxml/MainActivity.java
@@ -42,8 +42,12 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         if (savedInstanceState == null)
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, new YnetFragment())
-            .commit();
+        {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, new YnetFragment())
+                    .commit();
+        }
     }
 
     @Override
